407f23f8381afacca2f2a2978f16de20441b0a42
U-USERSAD\ptrinida <ptrinida@LIM-JTZXMF2.usersad.everis.int>
2019-12-04 23:18:42 -0500

    Nineth Microservice

---
+++ src/main/java/com/cashadvance/CashAdvanceMsApplication.java
@@ -0,0 +1,13 @@
+package com.cashadvance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashAdvanceMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashAdvanceMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cashadvance/CashAdvanceMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cashadvance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashAdvanceMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
